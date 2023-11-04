7e292fc6aad02578ff59beb77e9f4a74e54e74b3
henrillugany <76489997+henrillugany@users.noreply.github.com>
2023-11-04 17:34:25 +0100

    Update blackjack.py

--- BlackJack_game/blackjack.py
+++ BlackJack_game/blackjack.py
@@ -1,111 +1,67 @@
+Cette version du programme utilise des fonctions pour simplifier le code, permet de gérer les Aces de manière appropriée (1 point ou 11 points), et suit un flux plus logique pour le jeu. 
+
 # BLACK JACK - CASINO
 
 import random
 
-deck=[1,2,3,4,5,6,7,8,9,10,10,10,10,11]*4
+def deal_card(deck, hand, num_cards):
+    for _ in range(num_cards):
+        card = deck.pop()
+        hand.append(card)
+    return hand
 
+def calculate_total(hand):
+    total = sum(hand)
+    aces = hand.count(11)
+    while total > 21 and aces:
+        total -= 10
+        aces -= 1
+    return total
+
+def display_hand(hand, hide_first_card=False):
+    if hide_first_card:
+        visible_cards = ['X'] + hand[1:]
+    else:
+        visible_cards = hand
+    print("The cards are", visible_cards)
+    print("The total is", calculate_total(hand))
+
+def blackjack_winner(player_total, dealer_total):
+    if player_total > 21:
+        return "Dealer"
+    if dealer_total > 21:
+        return "Player"
+    if player_total == dealer_total:
+        return "Tie"
+    return "Player" if player_total > dealer_total else "Dealer"
+
+deck = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11] * 4
 random.shuffle(deck)
 
+dealer_hand = []
+player_hand = []
 
-print("                       **********************************************************                                    ")
-print("                                   Welcome to the game Casino - BLACK JACK !                                         ")
-print("                       **********************************************************                                    ")
+dealer_hand = deal_card(deck, dealer_hand, 2)
+player_hand = deal_card(deck, player_hand, 2)
 
+print("Welcome to the game Casino - BLACK JACK!")
+display_hand(dealer_hand, hide_first_card=True)
+display_hand(player_hand)
 
-d_cards = []                     #Initialising dealer's cards
-p_cards = []                     #Initialising player's cards
+if calculate_total(player_hand) == 21:
+    print("Blackjack! Player wins!")
+else:
+    while calculate_total(player_hand) < 21:
+        choice = input("Do you want to hit or stay? (1 for hit, 0 for stay): ")
+        if choice == "1":
+            player_hand = deal_card(deck, player_hand, 1)
+            display_hand(player_hand)
+        else:
+            break
 
-while len(d_cards) != 2:
-	random.shuffle(deck)
-	d_cards.append(deck.pop())
-	if len(d_cards) == 2:
-		print('The cards dealer has are X ',d_cards[1])
+    while calculate_total(dealer_hand) < 17:
+        dealer_hand = deal_card(deck, dealer_hand, 1)
 
-
-
-# Displaying the Player's cards
-while len(p_cards) != 2:
-	random.shuffle(deck)
-	p_cards.append(deck.pop())
-	if len(p_cards) == 2:
-		print("The total of player is ",sum(p_cards))
-		print("The cards Player has are  ",p_cards)
-
-
-if sum(p_cards) > 21:
-	print("You are BUSTED !\n  **************Dealer Wins !!******************\n")
-	exit()
-
-if sum(d_cards) > 21:
-	print("Dealer is BUSTED !\n   ************** You are the Winner !!******************\n")
-	exit()
-
-if sum(d_cards) == 21:
-	print("***********************Dealer is the Winner !!******************")
-	exit()
-
-if sum(d_cards) == 21 and sum(p_cards) == 21:
-	print("*****************The match is tie !!*************************")
-	exit()
-
-
-def dealer_choice():
-	if sum(d_cards) < 17:
-		while sum(d_cards) < 17:
-			random.shuffle(deck)
-			d_cards.append(deck.pop())	
-
-		
-	print("Dealer has total "+ str(sum(d_cards))+"with the cards ",d_cards)
-
-	if sum(p_cards) == sum(d_cards):
-		print("***************The match is tie !!****************")
-		exit()	
-
-
-	if sum(d_cards) == 21:
-		if sum(p_cards) < 21:
-		 print("***********************Dealer is the Winner !!******************") 
-		elif sum(p_cards) == 21:
-		 print("********************There is tie !!**************************")
-		else:
-		 print("***********************Dealer is the Winner !!******************")
-
-	elif sum(d_cards) <21:
-		if sum(p_cards) < 21 and sum(p_cards) < sum(d_cards):
-		 print("***********************Dealer is the Winner !!******************") 
-		if sum(p_cards) == 21:
-		 print("**********************Player is winner !!**********************")
-		if sum(p_cards) < 21 and sum(p_cards) > sum(d_cards):
-		 print("**********************Player is winner !!**********************") 
-
-	else:
-		if sum(p_cards) < 21:
-		 print("**********************Player is winner !!**********************") 
-		elif sum(p_cards) == 21:
-		 print("**********************Player is winner !!**********************")
-		else:
-		 print("***********************Dealer is the Winner !!******************")
-
-
-
-
-while sum(p_cards) < 21:
-
-    k = input('Want to hit or stay?\n Press 1 for hit and 0 for stay ')
-    if k == 1:
-        random.shuffle(deck)
-        p_cards.append(deck.pop())
-        print ('You have a total of ' + str(sum(p_cards))
-               + ' with the cards ', p_cards)
-        if sum(p_cards) > 21:
-            print ('*************You are BUSTED !*************\n Dealer Wins !!')
-        if sum(p_cards) == 21:
-            print ('*******************You are the Winner !!*****************************')
-
-			
-    else:
-	    dealer_choice()
-	    break
-        
-
+    display_hand(dealer_hand)
+    winner = blackjack_winner(calculate_total(player_hand), calculate_total(dealer_hand))
+    print("{winner} wins!")
